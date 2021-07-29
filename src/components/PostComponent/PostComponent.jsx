fc6f7a5d34c7404f65ed42307f76fcf1180d0821
Milos Srejic <srejicmilos@hotmail.com>
2021-07-29 23:32:08 +0200

    Added style for post component

--- src/components/PostComponent/PostComponent.jsx
+++ src/components/PostComponent/PostComponent.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './PostComponent.css';
 
 const PostComponent = (props) => {
-	// author: "Aleksandar";
-	// body: "This is the body of this first post and it have soem text in it";
-	// id: 1;
-	// title: "This is A first Post";
+  // author: "Aleksandar";
+  // body: "This is the body of this first post and it have soem text in it";
+  // id: 1;
+  // title: "This is A first Post";
 
-	return (
-		<div>
-			<h2>{props.title}</h2>
-			<h3>{props.author}</h3>
-			<Link to={`/posts/${props.id}`}>View Full Post</Link>
-		</div>
-	);
+  return (
+    <div className="post_component">
+      <h2>{props.title}</h2>
+      <h3>
+        <i>{props.author}</i>
+      </h3>
+      <Link to={`/posts/${props.id}`}>View Full Post</Link>
+    </div>
+  );
 };
 
 export default PostComponent;
