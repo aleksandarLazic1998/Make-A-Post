6608139d74cc0236614c7921acb6e089215a7cac
Milos Srejic <srejicmilos@hotmail.com>
2021-07-29 22:42:02 +0200

    Added style for detail post

--- src/Pages/NewPost/NewPost.jsx
+++ src/Pages/NewPost/NewPost.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import NewPostForm from '../../components/NewPostForm/NewPostForm';
 
 const NewPost = () => {
-<<<<<<< HEAD
   return (
     <div>
       {/* <h1>Add Post 🖋️</h1> */}
@@ -10,14 +9,6 @@ const NewPost = () => {
       <NewPostForm />
     </div>
   );
-=======
-	return (
-		<div>
-			<h1>Add Post 🖋️</h1>
-			<NewPostForm />
-		</div>
-	);
->>>>>>> e72dc5cdd5d65209a10f3f8838d48159c5040eb3
 };
 
 export default NewPost;
