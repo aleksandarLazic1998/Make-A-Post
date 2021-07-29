c2f42a483bbe40aebc4a356bbc1c257de5fcd687
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-07-29 14:51:58 +0200

    Added Header Component and Navigation with Some React-Router functionality

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from './Navigation/Navigation'
+
+function Header() {
+    return (
+        <div>
+            <h1>📑</h1>
+            <Navigation />
+        </div>
+    )
+}
+
+export default Header
