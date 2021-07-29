8f60733474e56b5636671c44b305627d4f7c86e0
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-07-29 17:41:29 +0200

    Added posibility to see full post with its text

--- src/Pages/PostDetail/PostDetail.jsx
+++ src/Pages/PostDetail/PostDetail.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const PostDetail = () =>{
-const params = useParams()
+const dummyItems = [
+	{
+		id: 1,
+		author: "Joka Lazic",
+		title: "Ovo je jokina price",
+		body: "Danas je mnogo vruc dan i bilo je lepo Arandjelovcu",
+	},
+	{
+		id: 2,
+		author: "tarMiRicmi",
+		title: "This is A Second Post",
+		body: "This is the body of this Second post and it have soem text in it",
+	},
+	{
+		id: 3,
+		author: "ACA Lazic",
+		title: "Mnogo mi je smor",
+		body: "Mnogo mi je smor a ja moram da ucim...",
+	},
+];
 
-    return (
-        <div>
-            {params.postId}
-        </div>
-    )
-}
+const PostDetail = () => {
+	const params = useParams();
 
-export default PostDetail
+	// Id from URL
+	const itemId = Number(params.postId);
+
+	// Item That will be rendered
+	const foundItem = dummyItems.find((item) => item.id === itemId);
+
+	return (
+		<div>
+			<h1>{foundItem.title}</h1>
+			<p>{foundItem.author}</p>
+			<p>{foundItem.body}</p>
+			{params.postId}
+		</div>
+	);
+};
+
+export default PostDetail;
